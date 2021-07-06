531ed9c46d5b120cafb4821a1c8ae4fc3ad03886
elisaj1317 <41582626+elisaj1317@users.noreply.github.com>
2021-07-06 16:50:05 -0700

    choose image feature

--- Instagram/View Controllers/ComposeViewController.m
+++ Instagram/View Controllers/ComposeViewController.m
@@ -7,7 +7,9 @@
 
 #import "ComposeViewController.h"
 
-@interface ComposeViewController ()
+@interface ComposeViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
 
 @end
 
@@ -16,6 +18,91 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapCancel:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)didTapShare:(id)sender {
+    
+}
+
+- (IBAction)didTapImage:(id)sender {
+    [self chooseSourceType];
+    
+}
+
+
+- (void)chooseSourceType {
+    // Create alert
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    // Create camera action
+    UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self imagePickerCamera:YES];
+    }];
+    [actionSheet addAction:cameraAction];
+    
+    // Create photo album action
+    UIAlertAction *cameraRollAction = [UIAlertAction actionWithTitle:@"Photo Album" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self imagePickerCamera:NO];
+    }];
+    [actionSheet addAction:cameraRollAction];
+    
+    // Create cancel action
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    [actionSheet addAction:cancelAction];
+    
+    // Display alert
+    [self presentViewController:actionSheet animated:YES completion:nil];
+    
+}
+
+- (void)imagePickerCamera: (BOOL)chooseCamera {
+    // set up image picker
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    // check if camera chosen and camera able
+    if (chooseCamera & [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:imagePickerVC animated:YES completion:nil];
+    }
+    else {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        // display error message before camera roll shows
+        if(chooseCamera){
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Camera is not available on this device. Photo Album will be shown." preferredStyle:UIAlertControllerStyleAlert];
+            
+            // create an OK action
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [self presentViewController:imagePickerVC animated:YES completion:nil];
+            }];
+            // add the OK action to the alert controller
+            [alert addAction:okAction];
+            
+            // display alert
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+        else {
+            [self presentViewController:imagePickerVC animated:YES completion:nil];
+        }
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+//    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.postImageView.image = editedImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 /*
