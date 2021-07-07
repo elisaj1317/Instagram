8b9f853f74753a3166bce43294d76cf70e723ea7
elisaj1317 <41582626+elisaj1317@users.noreply.github.com>
2021-07-07 14:52:52 -0700

    login and home auto layout

--- Instagram/Views/PostCell.h
+++ Instagram/Views/PostCell.h
@@ -12,7 +12,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PostCell : UITableViewCell
-
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 @property (weak, nonatomic) IBOutlet PFImageView *postImageView;
 @property (weak, nonatomic) IBOutlet UILabel *captionLabel;
 
--- Instagram/Views/PostCell.m
+++ Instagram/Views/PostCell.m
@@ -6,6 +6,7 @@
 //
 
 #import "PostCell.h"
+#import "DateTools.h"
 
 @implementation PostCell
 
@@ -23,10 +24,35 @@
 - (void)setPost:(Post *)post {
     _post = post;
     
-    self.captionLabel.text = post.caption;
+    [self setupLabels];
     
+    
+    // setup image
     self.postImageView.file = post.image;
     [self.postImageView loadInBackground];
+}
+
+- (void)setupLabels {
+    UIFont *boldFont = [UIFont fontWithName:@"HelveticaNeue-Bold" size:18];
+    UIFont *regularFont = [UIFont fontWithName:@"HelveticaNeue" size:18];
+    
+    // setup bolded username
+    NSMutableAttributedString *username = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@", self.post.author.username]];
+    [username addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0,username.length)];
+    
+    
+    self.usernameLabel.attributedText = username;
+    
+    
+    // setup caption
+    NSMutableAttributedString *captionText = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@" %@", self.post.caption]];
+    [captionText addAttribute:NSFontAttributeName value:regularFont range:NSMakeRange(0, captionText.length)];
+    
+    [username appendAttributedString:captionText];
+    self.captionLabel.attributedText = username;
+    
+    // setup date
+    self.timeLabel.text = self.post.createdAt.timeAgoSinceNow;
 }
 
 @end
